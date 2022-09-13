b5ea9d499bb1f9c3e2d8615c416ae935d65fb32d
gabrielrodriguesleite <gabrielrodriguesleite@gmail.com>
2022-09-12 23:19:06 -0300

    [feat] https request print body

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Web Crawler Go v1.0.0")
+
+	url := "https://aprendagolang.com.br"
+
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		panic("Status code diferente de 200")
+	}
+
+	fmt.Println("Body:", resp.Body)
 }
