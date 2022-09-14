3fd9f9b6fcc4f81d5ab81fa9cdca8579d4648ba5
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-09-13 21:19:57 -0300

    [doc] main clean commented deprecated code

--- main.go
+++ main.go
@@ -18,10 +18,7 @@ type VisitedLink struct {
 
 func main() {
 	fmt.Println("Web Crawler Go v1.0.0")
-
 	visitLink("https://aprendagolang.com.br")
-
-	// fmt.Println("Qtd links: ", len(links))
 }
 
 func visitLink(url string) {
@@ -37,13 +34,11 @@ func visitLink(url string) {
 		panic(fmt.Sprintf("[Erro] Status code diferente de 200: %v", resp.StatusCode))
 	}
 
-	// fmt.Println("Body:", resp.Body)
 	element, err := html.Parse(resp.Body)
 	if err != nil {
 		panic(fmt.Sprintf("Erro ao traduzir html: %v", err))
 	}
 
-	// fmt.Println("Body: ", body)
 	extractLinks(element)
 }
 
@@ -73,7 +68,6 @@ func extractLinks(element *html.Node) {
 			db.Insert("links", visitedLink)
 
 			visitLink(link.String())
-			// fmt.Println(attr.Val)
 		}
 	}
 
