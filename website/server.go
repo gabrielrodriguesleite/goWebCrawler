01a0d7c176ddb7f5faaf06790f6664857d6e07fd
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-09-13 23:46:41 -0300

    [feat] server load db data and provides to template

--- website/server.go
+++ website/server.go
@@ -1,10 +1,17 @@
 package website
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"text/template"
+
+	"github.com/gabrielrodriguesleite/goWebCrawler/db"
 )
+
+type DataLinks struct {
+	Links []db.VisitedLink
+}
 
 func Run() {
 	tmpl, err := template.ParseFiles("website/templates/index.html")
@@ -12,7 +19,13 @@ func Run() {
 		panic(err)
 	}
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl.Execute(w, nil)
+		links, err := db.FindAllLinks()
+		if err != nil {
+			fmt.Println("Erro ao carregar links: ", err)
+		}
+
+		data := DataLinks{Links: links}
+		tmpl.Execute(w, data)
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
