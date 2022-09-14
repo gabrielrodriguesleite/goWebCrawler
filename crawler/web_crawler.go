1d292328dd32cd2e9a59e2452645ee4ed3674a06
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-09-14 10:24:07 -0300

    [feat] package crawler created

---
+++ crawler/web_crawler.go
@@ -0,0 +1,66 @@
+package crawler
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"time"
+
+	"github.com/gabrielrodriguesleite/goWebCrawler/db"
+	"golang.org/x/net/html"
+)
+
+func VisitLink(url string) {
+	fmt.Printf("Visitando: %s\n", url)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(fmt.Sprintf("Não foi possível acessar: %s\nErro: %v", url, err))
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("[Erro] Status code diferente de 200: %v\n", resp.StatusCode)
+	}
+
+	element, err := html.Parse(resp.Body)
+	if err != nil {
+		panic(fmt.Sprintf("Erro ao traduzir html: %v", err))
+	}
+
+	extractLinks(element)
+}
+
+func extractLinks(element *html.Node) {
+	if element.Type == html.ElementNode && element.Data == "a" {
+		for _, attr := range element.Attr {
+			if attr.Key != "href" {
+				continue // continua buscando até achar href
+			}
+
+			link, err := url.Parse(attr.Val)
+			if err != nil || link.Scheme == "" || link.Scheme != "https" {
+				continue
+			}
+
+			if db.CheckVisitedLink(link.String()) {
+				fmt.Printf("link já visitado: %s\n", link)
+				continue
+			}
+
+			visitedLink := db.VisitedLink{
+				Website:     link.Host,
+				Link:        link.String(),
+				VisitedDate: time.Now(),
+			}
+
+			db.Insert("links", visitedLink)
+
+			go VisitLink(link.String())
+		}
+	}
+
+	for c := element.FirstChild; c != nil; c = c.NextSibling {
+		extractLinks(c)
+	}
+}
