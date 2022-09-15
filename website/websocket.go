e68f1a7350fd5985dcd9c5b62d3b35177fc858af
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-09-14 22:44:11 -0300

    [feat] websocket read write defined

--- website/websocket.go
+++ website/websocket.go
@@ -1,8 +1,14 @@
 package website
 
 import (
+	"context"
 	"html/template"
 	"net/http"
+	"time"
+
+	"github.com/gabrielrodriguesleite/goWebCrawler/crawler"
+	// "github.com/gorilla/websocket"
+	"nhooyr.io/websocket"
 )
 
 func websocketHandle() func(http.ResponseWriter, *http.Request) {
@@ -13,7 +19,48 @@ func websocketHandle() func(http.ResponseWriter, *http.Request) {
 
 	return func(w http.ResponseWriter, r *http.Request) {
 
+		if r.Method != http.MethodPost {
+			http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+			return
+		}
+
+		website := r.FormValue("website")
+		if website == "" {
+			http.Error(w, "O campo website não pode estar vazio", http.StatusBadRequest)
+			return
+		}
+
+		wc := crawler.New()
+		go wc.VisitLink(website)
+
+		c, err := websocket.Accept(w, r, nil)
+		if err != nil {
+			return
+		}
+
+		subcriber(r.Context(), c, wc.Log())
 		tmpl.Execute(w, nil)
 	}
 
+}
+
+func subcriber(ctx context.Context, c *websocket.Conn, logs <-chan string) error {
+	ctx = c.CloseRead(ctx)
+	for {
+		select {
+		case msg := <-logs:
+			err := writeTimeout(ctx, c, msg)
+			if err != nil {
+				return err
+			}
+		case <-ctx.Done():
+			return ctx.Err()
+		}
+	}
+}
+
+func writeTimeout(ctx context.Context, c *websocket.Conn, msg string) error {
+	ctx, cancel := context.WithTimeout(ctx, time.Second*3)
+	defer cancel()
+	return c.Write(ctx, websocket.MessageText, []byte(msg))
 }
