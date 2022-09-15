9dbfa4ae2c21448d10faeea0df0c71aae6f18693
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-09-14 22:17:37 -0300

    [feat] routes organizeds

--- website/server.go
+++ website/server.go
@@ -1,32 +1,13 @@
 package website
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"text/template"
-
-	"github.com/gabrielrodriguesleite/goWebCrawler/db"
 )
 
-type DataLinks struct {
-	Links []db.VisitedLink
-}
-
 func Run() {
-	tmpl, err := template.ParseFiles("website/templates/index.html")
-	if err != nil {
-		panic(err)
-	}
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		links, err := db.FindAllLinks()
-		if err != nil {
-			fmt.Println("Erro ao carregar links: ", err)
-		}
 
-		data := DataLinks{Links: links}
-		tmpl.Execute(w, data)
-	})
+	http.HandleFunc("/", indexHandle())
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
